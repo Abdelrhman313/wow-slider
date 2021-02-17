35c6f651f4ad93fa430f1033063ff0aa03d7d751
Abdelrhman_313 <abdelrhmanyasser77@gmail.com>
2021-02-17 22:58:48 +0200

    lastCommit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+
+jQuery("#wowslider-container6").wowSlider({effect:"flip",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+//***********************************************
+jQuery("#wowslider-container2").wowSlider({effect:"lines",prev:"",next:"",duration:20*100,delay:20*100,width:1400,height:600,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:true,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ******************************************************
+jQuery("#wowslider-container3").wowSlider({effect:"collage",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ************************************************************************
+jQuery("#wowslider-container4").wowSlider({effect:"cube",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// **********************************************************************
+jQuery("#wowslider-container5").wowSlider({effect:"page",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ************************************************************************
+jQuery("#wowslider-container7").wowSlider({effect:"slices",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ***********************************************************
+jQuery("#wowslider-container8").wowSlider({effect:"dribbles",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ********************************************************************
+jQuery("#wowslider-container9").wowSlider({effect:"brick",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ******************************************************************
+jQuery("#wowslider-container10").wowSlider({effect:"rotate",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ********************************************************************
+jQuery("#wowslider-container11").wowSlider({effect:"glass_parallax",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// *************************************************************************
+jQuery("#wowslider-container12").wowSlider({effect:"blinds",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ********************************************************************************
+jQuery("#wowslider-container14, #wowslider-container13").wowSlider({effect:"book",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+//**************************************************************************** 
+jQuery("#wowslider-container15").wowSlider({effect:"blur",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// *********************************************************
+jQuery("#wowslider-container16").wowSlider({effect:"squares",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// *********************************************************************
+jQuery("#wowslider-container17").wowSlider({effect:"stack_vertical",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ***********************************************************
+jQuery("#wowslider-container18").wowSlider({effect:"glass_parallax,seven",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ************************************************************************
+jQuery("#wowslider-container19").wowSlider({effect:"shift,louvers",prev:"",next:"",duration:20*100,delay:20*100,width:560,height:360,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+//************************************************************************ */ 
+jQuery("#wowslider-container20").wowSlider({effect:"domino,slices",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ****************************************************************************
+jQuery("#wowslider-container21").wowSlider({effect:"collage,fly",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// ***************************************************************************************
+jQuery("#wowslider-container22").wowSlider({effect:"cube_over,rotate",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
+
+// *********************************************************************************
+jQuery("#wowslider-container23").wowSlider({effect:"turn,tv",prev:"",next:"",duration:20*100,delay:20*100,width:360,height:480,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:1,onBeforeStep:0,images:0});
